9d9594edc2fd6c49086823018ed601991c5ae958
victor <victorreisprog@gmail.com>
2024-02-09 07:48:46 -0300

    feat: memory access etc

---
+++ memory_access_sync.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func memoryAccess1() {
+	var data int
+	go func() {
+		data++
+	}()
+	if data == 0 {
+		fmt.Printf("o valor é %v.\n", data)
+	}
+}
+
+func memoryAccess2() {
+	var memoryAccess sync.Mutex
+	var value int
+	go func() {
+		memoryAccess.Lock()
+		value++
+		memoryAccess.Unlock()
+	}()
+	memoryAccess.Lock()
+	if value == 0 {
+		fmt.Printf("o valor é %v.\n", value)
+		fmt.Printf("o valor é %v.\n", value)
+	}
+	memoryAccess.Unlock()
+}
